b40aa094b20912d3b977e063aab8712b664ef271
Shubham Kumar Gupta <shubham@cygnify.io>
2026-04-08 14:13:46 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// ============================================
+// CHANGE THE URL BELOW TO UPDATE YOUR QR CODE DESTINATION
+// After editing, commit the change and GitHub Pages
+// will automatically update within a few minutes.
+// ============================================
+
+var REDIRECT_URL = "https://example.com";
